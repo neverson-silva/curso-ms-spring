ad8c0885f179096b9bb5f8515fbc9946c3d9ec0f
Neverson Bento da Silva <neversonbs13@gmail.com>
2021-04-12 19:35:42 -0300

    Rest template

--- hr-payroll/src/main/java/com/neverson/hrpayroll/services/impl/PaymentServiceImpl.java
+++ hr-payroll/src/main/java/com/neverson/hrpayroll/services/impl/PaymentServiceImpl.java
@@ -1,15 +1,35 @@
 package com.neverson.hrpayroll.services.impl;
 
+import com.neverson.hrpayroll.entities.Worker;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 import com.neverson.hrpayroll.entities.Payment;
 import com.neverson.hrpayroll.services.PaymentService;
+import org.springframework.web.client.RestTemplate;
+
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @Service
 public class PaymentServiceImpl implements PaymentService{
-	
+
+	@Autowired
+	private RestTemplate restTemplate;
+
+	@Value("${hr-worker.host}")
+	private String workerHost;
+
 	public Payment getPayment(long workerId, int days) {
-		return new Payment("Bob", 200.00, days);
+
+		Map<String, String> params = new HashMap<>();
+		params.put("id", String.valueOf(workerId));
+
+		Worker worker = restTemplate.getForObject(workerHost + "/workers/{id}", Worker.class, params) ;
+
+		return new Payment(worker.getName(), worker.getDailyIncome(), days);
 	}
 
 }
