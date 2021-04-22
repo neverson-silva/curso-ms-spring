11da510ce6c505debd0e7c31b79685151b4ca9b9
Neverson Bento da Silva <neversonbs13@gmail.com>
2021-04-22 17:10:26 -0300

    hr-worker as a config server client

--- hr-worker/src/main/java/com/neverson/hrworker/resources/WorkerResource.java
+++ hr-worker/src/main/java/com/neverson/hrworker/resources/WorkerResource.java
@@ -5,6 +5,7 @@ import com.neverson.hrworker.services.WorkerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -26,6 +27,8 @@ public class WorkerResource {
     @Autowired
     private WorkerService service;
 
+    @Value("${test.config}")
+    private String config;
 
     @GetMapping
     public ResponseEntity<List<Worker>> findAll() {
@@ -53,6 +56,16 @@ public class WorkerResource {
         } else {
             return ResponseEntity.notFound().build();
         }
+
+    }
+
+    @GetMapping("/configs")
+    public ResponseEntity<Void> configs() {
+
+        logger.info("CONFIG = " + config);
+
+        return ResponseEntity.noContent().build();
+
 
     }
 
