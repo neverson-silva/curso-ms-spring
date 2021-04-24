c3bd8e3fd57a01b91d701719bd90fe879fe5cdd2
Neverson Bento da Silva <neversonbs13@gmail.com>
2021-04-24 12:07:02 -0300

    Configuração de CORS

--- hr-api-gateway-zuul/src/main/java/com/neverson/hrapigatewayzuul/config/ResourceServerConfig.java
+++ hr-api-gateway-zuul/src/main/java/com/neverson/hrapigatewayzuul/config/ResourceServerConfig.java
@@ -1,13 +1,23 @@
 package com.neverson.hrapigatewayzuul.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.servlet.FilterRegistrationBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.Ordered;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableResourceServer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.ResourceServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configurers.ResourceServerSecurityConfigurer;
 import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
+
+import java.util.Arrays;
+import java.util.Collections;
 
 @Configuration
 @EnableResourceServer
@@ -39,5 +49,28 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
             .anyRequest()
             .authenticated();
 
+        http.cors().configurationSource(corsConfigurationSource());
+    }
+
+    @Bean
+    public CorsConfigurationSource corsConfigurationSource() {
+        CorsConfiguration corsConfiguration = new CorsConfiguration();
+        corsConfiguration.setAllowedOrigins(Collections.singletonList("*"));
+        corsConfiguration.setAllowedMethods(Arrays.asList("POST", "PUT", "DELETE", "GET", "PATCH"));
+        corsConfiguration.setAllowCredentials(true);
+        corsConfiguration.setAllowedHeaders(Arrays.asList("Authorization", "Content-Type"));
+
+        UrlBasedCorsConfigurationSource urlSource = new UrlBasedCorsConfigurationSource();
+        urlSource.registerCorsConfiguration("/**", corsConfiguration);
+
+        return urlSource;
+    }
+
+    @Bean
+    public FilterRegistrationBean<CorsFilter> corsFilter() {
+        FilterRegistrationBean<CorsFilter> filterBean = new FilterRegistrationBean<>(
+                new CorsFilter(corsConfigurationSource()));
+        filterBean.setOrder(Ordered.HIGHEST_PRECEDENCE);
+        return filterBean;
     }
 }
