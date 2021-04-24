52e05798704d83d158fde24a79d29d3493bad6e9
Neverson Bento da Silva <neversonbs13@gmail.com>
2021-04-24 11:49:12 -0300

    Configuração de segurança para o servidor de configuração

--- hr-oauth/src/main/java/com/neverson/hroauth/config/AppConfig.java
+++ hr-oauth/src/main/java/com/neverson/hroauth/config/AppConfig.java
@@ -1,5 +1,6 @@
 package com.neverson.hroauth.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -9,15 +10,19 @@ import org.springframework.security.oauth2.provider.token.store.JwtTokenStore;
 @Configuration
 public class AppConfig {
 
+    @Value("${jwt.secret}")
+    private String jwtSecret;
+
     @Bean
     public BCryptPasswordEncoder bCryptPasswordEncoder() {
         return new BCryptPasswordEncoder();
     }
 
+
     @Bean
     public JwtAccessTokenConverter accessTokenConverter() {
         JwtAccessTokenConverter tokenConverter = new JwtAccessTokenConverter();
-        tokenConverter.setSigningKey("appsigningkey");
+        tokenConverter.setSigningKey(jwtSecret);
         return tokenConverter;
     }
 
